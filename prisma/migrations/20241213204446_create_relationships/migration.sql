45cd325b32a445ba499c787edca7d75843480f63
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-12-13 17:44:55 -0300

    Cria relacionamentos

---
+++ prisma/migrations/20241213204446_create_relationships/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `users` table. All the data in the column will be lost.
+  - Added the required column `gym_id` to the `check_ins` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `check_ins` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `gyms` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `hashed_password` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "check_ins" ADD COLUMN     "gym_id" TEXT NOT NULL,
+ADD COLUMN     "user_id" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "gyms" ADD COLUMN     "user_id" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "password",
+ADD COLUMN     "hashed_password" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_gym_id_fkey" FOREIGN KEY ("gym_id") REFERENCES "gyms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "gyms" ADD CONSTRAINT "gyms_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
